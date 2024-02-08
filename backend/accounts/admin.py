fc52fec5baceeee84aeba9234f214b583a87bcbf
Viktor <vvlasovdo@mail.ru>
2024-02-08 16:29:52 +0900

    ref user model

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,4 +1,63 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User
-admin.site.register(User)
-# Register your models here.
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {"fields": ("first_name", "last_name", "middle_name")},
+        ),
+        (
+            "Contact info",
+            {
+                "fields": (
+                    "email",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+    )
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "is_staff",
+    )
+    search_fields = ("username", "last_name", "first_name", "middle_name")
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,16 +1,53 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    UserManager,
+)
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils import timezone
 
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+    username_validator = UnicodeUsernameValidator()
 
-class User(AbstractUser):
+    username = models.CharField(
+        "username",
+        max_length=150,
+        unique=True,
+        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+        validators=[username_validator],
+        error_messages={
+            "unique": "A user with that username already exists.",
+        },
+    )
+    email = models.EmailField("email address", blank=True, null=True)
+    
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100)
-    avatar = models.ImageField(upload_to='media/accounts/avatars/', blank=True, default='')
+    avatar = models.ImageField(upload_to='media/accounts/avatars/', blank=True, default='avatar.svg')
+    
+    is_staff = models.BooleanField(
+        "staff status",
+        default=False,
+        help_text="Designates whether the user can log into this admin site."
+    )
+    is_active = models.BooleanField(
+        "active",
+        default=True,
+        help_text="Designates whether this user should be treated as active. "
+    )
+    date_joined = models.DateTimeField("date joined", default=timezone.now)
+
+    USERNAME_FIELD = "username"
 
     @property
     def full_name(self):
-        return f'{self.first_name} {self.middle_name} {self.last_name}'.replace('  ',' ')
+        if self.first_name and self.last_name:
+            return f'{self.first_name} {self.middle_name} {self.last_name}'.replace('  ',' ')
+        else:
+            return f'{self.username}'.replace('  ',' ')
     
     def __str__(self):
         return f'{self.id} - {self.full_name}'
